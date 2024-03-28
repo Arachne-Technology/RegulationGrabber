13772abef800d1c9fa7a2e417c0c0ea75f5c0a7f
Caitlyn Byrne <cbyrne@arachne.us>
2024-03-28 10:23:32 -0400

    Refactor how environment config is read and passed to functions

--- usg_doc_retriever.py
+++ usg_doc_retriever.py
@@ -24,45 +24,56 @@ from bs4 import BeautifulSoup
 logging.basicConfig(level=logging.DEBUG) # REMOVE ME LATER
 logger = logging.getLogger(__name__)
 
+def get_environment_config() -> dict[str, str]:
+    """Reads the ini file
 
-def download_documents_index() -> None:
-    """US Gov. Doc Retriever
-
-    Goes to the Acquisition website, and downloads the latest copies
-    of the acquisition regulations.  Parses them according to each 
-    regulation.
+    returns the following values, as a dictionary:
+    'regulation_storage_path'
+    'regulation_base_url'
+    'temp_dir'
     """
 
     # Open up the configuration
     parser = ConfigParser()
     parser.read('config.ini')
 
+    retval = {}
     try:
-        regulation_storage_path = parser.get('Local', 'Regulation_Local_Location')
+        retval['regulation_storage_path'] = parser.get('Local', 'Regulation_Local_Location')
     except ConfigParserError:
         # Catch all configparser errors
         logger.error("Missing Configuration Item For "
                      "Regulation Storage Path:  Local -> "
                      "Regulation_Local_Location")
-        regulation_storage_path = "raw_regulations"  # Default Value
-    logger.info("Regulation Storage Path: %s", os.path.abspath(regulation_storage_path))
+        retval['regulation_storage_path'] = "raw_regulations"  # Default Value
+    logger.debug("Regulation Storage Path: %s", os.path.abspath(retval['regulation_storage_path']))
 
     try:
-        regulation_base_url = parser.get("Acquisition_Regulations", "Base_URL")
+        retval['regulation_base_url'] = parser.get("Acquisition_Regulations", "Base_URL")
     except ConfigParserError:
         # Catch all configparser errors
         logger.error("Missing Configuration Item for Acquisition.gov URL: "
                      " Acquisition_Regulations -> Base_URL")
-        regulation_base_url = "https://www.acquisition.gov/content/regulations"
-    logger.info("Pulling Regulations from <%s>", regulation_base_url)
+        retval['regulation_base_url'] = "https://www.acquisition.gov/content/regulations"
+    logger.debug("Pulling Regulations from <%s>", retval['regulation_base_url'])
 
     try:
-        temp_dir = parser.get("Local", "Temp_Dir")
+        retval['temp_dir'] = parser.get("Local", "Temp_Dir")
     except ConfigParserError:
         # Catch all configparser errors:
         logger.error("Missing Configuration Item for Temp Directory:  Local -> Temp_Dir")
-        temp_dir = "temp"
-    logger.debug("Temp file path: %s", os.path.abspath(temp_dir))
+        retval['temp_dir'] = "temp"
+    logger.debug("Temp file path: %s", os.path.abspath(retval['temp_dir']))
+
+    return retval
+
+def download_documents_index(regulation_storage_path, regulation_base_url, temp_dir) -> None:
+    """US Gov. Doc Retriever
+
+    Goes to the Acquisition website, and downloads the latest copies
+    of the acquisition regulations.  Parses them according to each 
+    regulation.
+    """
 
     # Make temp dir if needed
     if not os.path.isdir(temp_dir):
@@ -138,14 +149,18 @@ def download_documents_index() -> None:
 
     # print(regulation_dict)
                 
-def update_documents() -> None:
-    NotImplemented
+def update_documents(regulation_storage_path, regulation_base_url, temp_dir) -> None:
+    regulation_index_csv_filename = os.path.join(
+        regulation_storage_path, "metadata.csv"
+    )
+    
 
 def main() -> None:
     """Runs tests for the doc retriever"""
     logger.setLevel(logging.DEBUG)
-    download_documents_index()
-    update_documents()
+    envconf = get_environment_config()
+    download_documents_index(**envconf)
+    update_documents(**envconf)
 
 
 if __name__ == "__main__":
